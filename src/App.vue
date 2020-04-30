2606c34e4ac6054bddc0c6a76b9317fcfb196f8a
taramonheim <taramonheim@me.com>
2020-04-30 22:04:50 +0200

    Vue.Js Server Zugriff

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Plan msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Plan from  './views/Plan.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+ components: {
+   Plan
   }
 }
 </script>
---
+++ src/views/Plan.vue
@@ -0,0 +1,53 @@
+<template>
+<body>
+  <h1>Full Stack Project</h1>
+  <label for="Weekdays"><i>Choose a day</i></label>
+  <br>
+  <select id="Weekdays">
+    <option value="monday">Monday</option>
+    <option value="tuesday">Tuesday</option>
+    <option value="wednesday">Wednesday</option>
+    <option value="thursday">Thursday</option>
+    <option value="friday">Friday</option>
+  </select>
+
+  <br>
+  <br>
+  <br>
+  <table>
+    <tr>
+      <th>Information</th>
+      <th>Costs</th>
+      <th>Day</th>
+      <th>Downvotes</th>
+      <th>Id</th>
+      <th>Labels</th>
+      <th>Name</th>
+      <th>Upvotes</th>
+    </tr>
+    <tr>
+      <td>*</td>
+      <td>*</td>
+      <td>*</td>
+      <td>*</td>
+      <td>*</td>
+      <td>*</td>
+      <td>*</td>
+      <td>*</td>
+    </tr>
+  </table>
+  </body>
+</template>
+
+<script>
+  export default {
+    name: 'Plan',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
